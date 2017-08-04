d3b5f3fc0aba7718a7e6a5c14529422af47dec2f
kostas <kostasder94x@gmail.com>
2017-08-04 08:54:27 +0300

    Hangling Peers messages

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
 
 import System.Environment
@@ -9,60 +11,129 @@ import Control.Monad
 import Network
 import System.IO
 
-
 import Lib
 
-data Peer = Peer HostName PortNumber deriving (Read, Show)
+-- Peer interface --
 
-type Tx = Int
+data Peer = 
+  Peer 
+    { hostName :: HostName 
+    , port     :: PortNumber 
+    } 
+  deriving (Read, Show)
+
+type Peers = [Peer]
+
+newPeers :: IO (TVar Peers)
+newPeers = do
+  p <- newTVarIO []
+  return p
+
+-- End of Peer interface --
 
 data Message =
     Connect HostName PortNumber
   | GetPeers
-  | Status [Peer]
+  | Status Peers
   | Newtx Tx
   | Oldtx Tx Tx
   | Quit
   | Unknown String
   deriving (Read, Show)
 
+-- Transactions Interface -- 
 
-newPeers :: IO (TVar [Peer])
-newPeers = do
+type Tx = Int
+type Transactions = [Tx]
+newTransactions :: IO (TVar Transactions)
+newTransactions = do
   p <- newTVarIO []
-  return p
+  return p 
+
+inNew :: Transactions -> Tx -> Bool
+inNew [] tx = False
+isNew (x:xs) tx = tx == x
+
+getNew :: Transactions -> Tx
+getNew = head 
+
+--
+-- STM Nothing   means that a new Transaction was found. Insert it!
+-- STM (Just tx) means that this transactions exists/is old. Return the newest!
+--
+
+processNewTx :: Tx -> TVar Transactions -> STM (Maybe Tx)
+processNewTx newtx txs' = do
+  txs <- readTVar txs'
+  if isNew txs newtx
+  then do    
+    writeTVar txs' (newtx:txs)
+    return Nothing
+  else
+    return $ Just $ getNew txs
+
+-- End of Transaction Interface -- 
 
 main :: IO ()
 main = do
-  peers <- newPeers
+  gpeers <- newPeers --global Peers TVar
+  gtxs <- newTransactions --global Transactions TVar
+  newTransactions 
   args <- getArgs
-  let ip = head
-      port = read $ head $ tail args
+  let port = read $ head args
+      seedIp = head $ tail args
+      seedPort = read $ head $ tail $ tail args
+  forkIO $ startUpThread seedIp $ PortNumber seedPort 
+  forkIO $ randomIntervals gpeers gtxs
   s <- listenOn (PortNumber port)
   putStrLn $ "Listening on port " ++ show port
   forever $ do
     hhp <- accept s
-    forkIO $ clientThread hhp peers
+    forkIO $ clientThread hhp gpeers gtxs --TODO handle failures/exceptions/closed connections etc
 
+startUpThread :: HostName -> PortID -> IO ()
+startUpThread h cport = undefined 
+--TODO connect to Node to find new peers on startup
+--Send Newtx 0 messages
 
-clientThread ::(Handle, HostName, PortNumber) -> TVar [Peer] -> IO ()
-clientThread (h, chost, cport) peers = do 
+randomIntervals :: TVar Peers -> TVar Transactions -> IO () 
+randomIntervals gpeers gtxs = undefined -- TODO wake up every 10s to create new txs
+
+clientThread :: (Handle, HostName, PortNumber) -> TVar Peers -> TVar Transactions -> IO ()
+clientThread (h, chost, cport) peers txs = do 
   putStrLn $ "Accepted connection from " ++ show chost ++ ":" ++ show cport
   hSetNewlineMode h universalNewlineMode
   hSetBuffering h LineBuffering
   forever $ do
     msg <- hGetLine h
-    dosth h peers $ read msg
+    processMessage h peers txs $ read msg
 
-dosth :: Handle -> TVar [Peer] -> Message -> IO ()
-dosth h peers = go
+processMessage :: Handle -> TVar Peers -> TVar Transactions -> Message -> IO ()
+processMessage h gpeers gtxs = go
   where
     go (Connect host port) = do
-      atomically $ modifyTVar' peers (\old -> (Peer host port):old)
+      atomically $ modifyTVar' gpeers (\old -> (Peer host port):old)
       return ()
     go GetPeers = do
-      p <- atomically $ readTVar peers -- ?
+      p <- atomically $ readTVar gpeers -- ?
       hPutStrLn h $ show $ Status p
-    go (Newtx tx) = error ""
+    go (Newtx tx) = do
+      maybeTx <- atomically $ processNewTx tx gtxs
+      case maybeTx of
+        Nothing -> propagateToPeers tx --TODO log
+        Just newestTxKnown -> hPutStrLn h $ show $ Oldtx newestTxKnown tx
+    go Quit = error "Quit Message uniplemented" -- TODO
+    go (Unknown str) = error "Unknown Message uniplemented" -- TODO
+    
+    propagateToPeers :: Tx -> IO () 
+    propagateToPeers tx = do
+      peers <- atomically $ readTVar gpeers
+      let msg = Newtx tx
+      sequence $ map (send msg) peers -- TODO Add delays between send
+      return ()
 
+send :: Message -> Peer -> IO ()
+send msg Peer{..} = undefined --TODO use chans for each peer (add to record) . Else just open a new connection
+  
+  
 
