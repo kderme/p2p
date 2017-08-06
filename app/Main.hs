d8d727358247e0e2fafde47cf8da7bb753e2573d
Alex Moschos <alexgmoschos@gmail.com>
2017-08-06 16:13:27 +0300

    Added delays, randomIntervals and fixed tx interface

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,7 @@ import Control.Concurrent.STM
 import Control.Exception
 import Control.Monad
 import Data.Time
-
+import System.Random
 import Network
 import System.IO
 
@@ -16,11 +16,11 @@ import Lib
 
 -- Peer interface --
 
-data Peer = 
-    Peer 
-        { hostName :: HostName 
-        , port     :: PortNumber 
-        } 
+data Peer =
+    Peer
+        { hostName :: HostName
+        , port     :: PortNumber
+        }
     deriving (Read, Show)
 
 type Peers = [Peer]
@@ -40,19 +40,21 @@ data Message =
     | Unknown String
     deriving (Read, Show)
 
--- Transactions Interface -- 
+-- Transactions Interface --
 
 type Tx = Int
 type Transactions = [Tx]
 newTransactions :: IO (TVar Transactions)
-newTransactions = newTVarIO [] 
+newTransactions = newTVarIO []
 
 isNew :: Transactions -> Tx -> Bool
 isNew [] tx = True
-isNew (x:xs) tx = tx == x
+isNew (x:xs) tx
+    | tx > x    = True
+    | otherwise = False
 
 getNew :: Transactions -> Tx
-getNew = head 
+getNew = head
 
 --
 -- STM Nothing   means that a new Transaction was found. Insert it!
@@ -63,13 +65,15 @@ processNewTx :: Tx -> TVar Transactions -> STM (Maybe Tx)
 processNewTx newtx txs' = do
     txs <- readTVar txs'
     if isNew txs newtx
-    then do    
+    then do
         writeTVar txs' (newtx:txs)
         return Nothing
     else
         return $ Just $ getNew txs
 
--- End of Transaction Interface -- 
+-- End of Transaction Interface --
+second :: Int
+second = 1000000
 
 main :: IO ()
 main = do
@@ -80,34 +84,50 @@ main = do
     let port = read $ head args
         seedIp = head $ tail args
         seedPort = read $ head $ tail $ tail args
+        delay = read $ head $ tail $ tail $ tail args
         logfile = "txos_log"
     forkIO $ startUpThread seedIp $ PortNumber seedPort
-    forkIO $ randomIntervals gpeers gtxs
+    forkIO $ randomIntervals gpeers gtxs logfile
     s <- listenOn (PortNumber port)
     putStrLn $ "Listening on port " ++ show port
     forever $ do
         hhp <- accept s
-        forkIO $ clientThread hhp gpeers logfile gtxs --TODO handle failures/exceptions/closed connections etc
+        forkIO $ clientThread hhp gpeers logfile delay gtxs --TODO handle failures/exceptions/closed connections etc
 
 startUpThread :: HostName -> PortID -> IO ()
 startUpThread h cport = undefined
 --TODO connect to Node to find new peers on startup
 --Send Newtx 0 messages
 
-randomIntervals :: TVar Peers -> TVar Transactions -> IO ()
-randomIntervals gpeers gtxs = undefined -- TODO wake up every 10s to create new txs
+randomIntervals :: TVar Peers -> TVar Transactions -> FilePath -> IO ()
+randomIntervals gpeers gtxs logfile = forever $ do
+    interval <- randomRIO(10,120)
+    print interval
+    threadDelay (interval*second)
+    putStrLn $ "I just waited for " ++ show interval ++ "second"
+    offset <- randomRIO(1,10)
+    txs <- atomically $ readTVar gtxs
+    peers <- atomically $ readTVar gpeers
+    let recent = if null txs then 0 else head txs
+    atomically $ processNewTx (recent+offset) gtxs
+    -- TODO: log the new transaction
+    timestamp <- getCurrentTime
+    appendFile logfile ("Tx #: " ++ show (recent+offset) ++ " from me" ++ " " ++ show timestamp ++ "\n")
+    --putStrLn "I am going to send now"
+    mapM_ (send (Newtx (recent+offset))) peers
 
-clientThread :: (Handle, HostName, PortNumber) -> TVar Peers -> FilePath -> TVar Transactions -> IO ()
-clientThread (h, chost, cport) peers logfile txs = do
+
+clientThread :: (Handle, HostName, PortNumber) -> TVar Peers -> FilePath -> Int -> TVar Transactions -> IO ()
+clientThread (h, chost, cport) peers logfile delay txs = do
     putStrLn $ "Accepted connection from " ++ show chost ++ ":" ++ show cport
     hSetNewlineMode h universalNewlineMode
     hSetBuffering h LineBuffering
     forever $ do
         msg <- hGetLine h
-        processMessage h chost peers txs logfile $ read msg
+        processMessage h chost peers txs logfile delay (read msg)
 
-processMessage :: Handle -> HostName -> TVar Peers -> TVar Transactions -> FilePath -> Message -> IO ()
-processMessage h chost gpeers gtxs logfile = go
+processMessage :: Handle -> HostName -> TVar Peers -> TVar Transactions -> FilePath -> Int -> Message -> IO ()
+processMessage h chost gpeers gtxs logfile delay  = go
   where
     go (Connect host port) = do
         atomically $ modifyTVar' gpeers (\old -> Peer host port:old)
@@ -131,11 +151,11 @@ processMessage h chost gpeers gtxs logfile = go
     propagateToPeers tx = do
         peers <- atomically $ readTVar gpeers
         let msg = Newtx tx
+        --putStrLn "I will wait"
+        threadDelay(delay*second)
+        --putStrLn "I waited"
         mapM_ (send msg) peers -- TODO Add delays between send
 
 
 send :: Message -> Peer -> IO ()
-send msg Peer{..} = undefined --TODO use chans for each peer (add to record) . Else just open a new connection
-  
-  
-
+send msg Peer{..} = error "send is not implemented yet" --TODO use chans for each peer (add to record) . Else just open a new connection
